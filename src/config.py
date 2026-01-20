788c89b59adce348725afb4042158fbb35d61542
wanghaohong <wanghaohong318@qq.com>
2026-01-20 21:41:22 +0800

    更新夏天模板配置

--- src/config.py
+++ src/config.py
@@ -40,6 +40,29 @@ SummerSetsConfig = {
             ],
             "outputJpgFileName": "{0}_{1}", 
         },
+        "b1": { 
+            "desc": "短袖上衣正面无背带短裤正面",
+            "CompositeElements": [
+                # 合成部分
+                {
+                    "desc": "从对应品牌中筛选出短袖上衣",
+                    "type": "shirts", # 对应 SingleGoodsPngObj 中的一个类型或者shirts文件夹名，output中用用shirts的子文件夹名进行分文件夹
+                    "fileNamePattern": r'(\d+)_0\.png',
+                    "zOrder": 1, # 合图时候的顺序，数字越小越在下面，反之则越在上面，默认是和key一样
+                    "scale": 0.975,
+                    "pos": [275, 342],
+                },
+                {
+                    "desc": "从对应品牌中筛选出短裤背面",
+                    "type": "shorts", # 对应 SingleGoodsPngObj 中的一个类型或者shirts文件夹名，output中用用shirts的子文件夹名进行分文件夹
+                    "fileNamePattern": r'(\d+)_0\.png',
+                    "zOrder": 0, # 合图时候的顺序，数字越小越在下面，反之则越在上面，默认是和key一样
+                    "scale": 0.53,
+                    "pos": [650, 548],
+                },
+            ],
+            "outputJpgFileName": "{0}_{1}", 
+        },
         "c": { 
             "desc": "短袖上衣后背面背带短裤",
             "CompositeElements": [
@@ -63,6 +86,29 @@ SummerSetsConfig = {
             ],
             "outputJpgFileName": "{0}_{1}", 
         },
+        "c1": { 
+            "desc": "短袖上衣正面背带短裤正面",
+            "CompositeElements": [
+                # 合成部分
+                {
+                    "desc": "从对应品牌中筛选出短袖上衣",
+                    "type": "shirts",
+                    "fileNamePattern": r'(\d+)_0\.png',
+                    "zOrder": 1,
+                    "scale": 1.0,
+                    "pos": [280, 400],
+                },
+                {
+                    "desc": "从对应品牌中筛选出短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'(\d+)_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.185,
+                    "pos": [658, 501],
+                },
+            ],
+            "outputJpgFileName": "{0}_{1}", 
+        },
         "d": { 
             "desc": "背带短裤背面",
             "CompositeElements": [
@@ -70,6 +116,20 @@ SummerSetsConfig = {
                     "desc": "从对应品牌中筛选出短裤背面",
                     "type": "bibShorts",
                     "fileNamePattern": r'(\d+)_1\.png',
+                    "zOrder": 0,
+                    "scale": 1,
+                    "pos": [400, 400],
+                },
+            ],
+            "outputJpgFileName": "{0}_1", 
+        },
+        "d1": { 
+            "desc": "背带短裤正面",
+            "CompositeElements": [
+                {
+                    "desc": "从对应品牌中筛选出短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'(\d+)_0\.png',
                     "zOrder": 0,
                     "scale": 1,
                     "pos": [400, 400],
@@ -90,6 +150,20 @@ SummerSetsConfig = {
                 },
             ],
             "outputJpgFileName": "{0}_1", 
+        },
+        "e1": { 
+            "desc": "无背带短裤正面",
+            "CompositeElements": [
+                {
+                    "desc": "无背带短裤正面",
+                    "type": "shorts",
+                    "fileNamePattern": r'(\d+)_0\.png',
+                    "zOrder": 0,
+                    "scale": 1,
+                    "pos": [400, 400],
+                },
+            ],
+            "outputJpgFileName": "{0}_0", 
         },
         "v1": { 
             "desc": "两件马甲无背面",
@@ -279,6 +353,52 @@ SummerSetsConfig = {
             ],
             "outputJpgFileName": "两件短袖套装", 
         },
+        "s11": { 
+            "desc": "两件短袖套装",
+            "CompositeElements": [
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [534, 236],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [213, 424],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "shorts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.33,
+                    "pos": [398, 693],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.135,
+                    "pos": [557, 607],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.135,
+                    "pos": [704, 607],
+                }
+            ],
+            "outputJpgFileName": "两件短袖套装", 
+        },
         "s2": { 
             "desc": "两件短袖套装2",
             "CompositeElements": [
@@ -318,6 +438,52 @@ SummerSetsConfig = {
                     "desc": "短裤背面",
                     "type": "bibShorts",
                     "fileNamePattern": r'4_1\.png',
+                    "zOrder": 0,
+                    "scale": 0.138,
+                    "pos": [722, 593],
+                }
+            ],
+            "outputJpgFileName": "两件短袖套装2", 
+        },
+        "s21": { 
+            "desc": "两件短袖套装2",
+            "CompositeElements": [
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [534, 236],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [213, 424],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "shorts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.365,
+                    "pos": [410, 663],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.138,
+                    "pos": [577, 592],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'4_0\.png',
                     "zOrder": 0,
                     "scale": 0.138,
                     "pos": [722, 593],
@@ -379,6 +545,60 @@ SummerSetsConfig = {
             ],
             "outputJpgFileName": "三件短袖套装", 
         },
+        "s31": { 
+            "desc": "三件短袖套装",
+            "CompositeElements": [
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [607, 319],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [387, 314],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [168, 317],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "shorts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.33,
+                    "pos": [398, 693],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.135,
+                    "pos": [557, 607],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.135,
+                    "pos": [704, 607],
+                }
+            ],
+            "outputJpgFileName": "三件短袖套装", 
+        },
         "s4": { 
             "desc": "三件短袖套装2",
             "CompositeElements": [
@@ -426,6 +646,60 @@ SummerSetsConfig = {
                     "desc": "短裤背面",
                     "type": "bibShorts",
                     "fileNamePattern": r'4_1\.png',
+                    "zOrder": 0,
+                    "scale": 0.138,
+                    "pos": [722, 593],
+                }
+            ],
+            "outputJpgFileName": "三件短袖套装2", 
+        },
+        "s41": { 
+            "desc": "三件短袖套装2",
+            "CompositeElements": [
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [607, 319],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [387, 314],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [168, 317],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "shorts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.365,
+                    "pos": [410, 663],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.138,
+                    "pos": [577, 592],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'4_0\.png',
                     "zOrder": 0,
                     "scale": 0.138,
                     "pos": [722, 593],
@@ -495,6 +769,68 @@ SummerSetsConfig = {
             ],
             "outputJpgFileName": "四件短袖套装", 
         },
+        "s51": { 
+            "desc": "四件短袖套装",
+            "CompositeElements": [
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [607, 230],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [460, 303],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [335, 401],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'4_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [168, 496],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "shorts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.33,
+                    "pos": [398, 693],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.135,
+                    "pos": [557, 607],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.135,
+                    "pos": [704, 607],
+                }
+            ],
+            "outputJpgFileName": "四件短袖套装", 
+        },
         "s6": { 
             "desc": "四件短袖套装2",
             "CompositeElements": [
@@ -550,6 +886,68 @@ SummerSetsConfig = {
                     "desc": "短裤背面",
                     "type": "bibShorts",
                     "fileNamePattern": r'4_1\.png',
+                    "zOrder": 0,
+                    "scale": 0.138,
+                    "pos": [722, 593],
+                }
+            ],
+            "outputJpgFileName": "四件短袖套装2", 
+        },
+        "s61": { 
+            "desc": "四件短袖套装2",
+            "CompositeElements": [
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'1_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [607, 230],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [460, 303],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [335, 401],
+                },
+                {
+                    "desc": "短袖正面",
+                    "type": "shirts",
+                    "fileNamePattern": r'4_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.64,
+                    "pos": [168, 496],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "shorts",
+                    "fileNamePattern": r'2_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.365,
+                    "pos": [410, 663],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'3_0\.png',
+                    "zOrder": 0,
+                    "scale": 0.138,
+                    "pos": [577, 592],
+                },
+                {
+                    "desc": "短裤背面",
+                    "type": "bibShorts",
+                    "fileNamePattern": r'4_0\.png',
                     "zOrder": 0,
                     "scale": 0.138,
                     "pos": [722, 593],
